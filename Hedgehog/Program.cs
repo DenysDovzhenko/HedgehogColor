e42426ca7ab7ca36849ce9165a565e4bb41f233c
Denys Dovzhenko <denis.dovzhenko1@gmail.com>
2024-02-08 11:39:19 +0200

    Add project files.

---
+++ Hedgehog/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Hedgehog
+{
+    enum colors
+    {
+        Red,
+        Green,
+        Blue
+    }
+    class Hedgehog
+    {
+        public int color {  get; set; }
+        public Hedgehog(int color)
+        {
+            this.color = color; 
+        }
+        public void changeColor(Hedgehog otherHedgehog) 
+        { 
+            if (this.color != otherHedgehog.color)
+            {
+
+            }
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
